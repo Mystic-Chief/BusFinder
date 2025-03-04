5ff061968e4bc042e4510093b2a906d4853463ac
Mystic-Chief <107339137+Mystic-Chief@users.noreply.github.com>
2025-03-04 17:56:48 +0530

    Frontend Production Build Logic Fic

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,12 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: { drop_console: true }
+export default defineConfig(({ mode }) => {
+  return {
+    define: {
+      'process.env.NODE_ENV': JSON.stringify(mode === 'production' ? 'production' : 'development')
+    },
+    plugins: [react()],
+    build: {
+      minify: 'terser',
+      terserOptions: {
+        compress: { drop_console: true }
+      }
     }
-  }
-})
+  };
+});
